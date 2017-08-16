886844cd48f9cae757be280eb561d1111b8f2f78
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-08-16 16:05:46 -0700

    updates

--- src/commands/autocomplete/options.js
+++ src/commands/autocomplete/options.js
@@ -18,102 +18,67 @@ export default class ACFoo extends AutocompleteBase {
   parsedFlags: {[name: string]: ?string} = {}
 
   async run () {
-    // A gather input
-    // B get needed data
-    //  1. get cmd to hydrate
-    //  2. set argv
-    // C determine want
-    //  see below...
-    // D determine context
-    //  1. args remaining & complete-able?
-    //  2. flags remaining & complete-able?
-    // E from want & context, return wanted
-
-    // WANT --
-    // WE ONLY RETURN ARG/FLAG VALUE COMPELETIONS
-    // (not short/long flag char/name)
-    // so the "want" can only be:
-    //  1. the current arg value completion
-    //  2. the current flag value completion
-    //  (& it may be asking in error, ex: for flag name or nothing left to compelete)
-
-    //
-    // First question is, what is the curArg?
-
-    // all curArg types:
-    // 1) undefined (ERROR)
-    // 2) ''
-    // 3) myar <- semi-completed arg value
-    // 4) myfl <- semi-completed flag value
-    // 5) --foo=myfl <- semi-completed long= flag value
-    // 6) -* || --f* <- semi-completed flag name (ERROR)
-
-    // 1) if curArg undefined, beep/exit
-
-    // 2) if curArg is ''
-    // can only be:
-    // a. cur = nth arg completion
-    // b. cur = cur-1 flag value completion
-    // c. cur = beep/end-of-available-completions
-
-    // 3/4/5/6) if curArg is a not ''
-    // we are in the middle of a completion
-    // 6) if curArg is semi-completed flag name, beep/exit
-
+    // ex: heroku autocomplete:options 'heroku addons:destroy -a myapp myaddon'
     try {
-      // A
-      const cliArgv = this.argv[0].split(' ')
-      const argv = cliArgv.slice(2)
+      // A - grab cmd line to complete from argv
+      const commandLineToComplete = this.argv[0].split(' ')
 
-      // B
-      const curArg = argv[argv.length - 1]
-      const previousArg = argv[argv.length - 2]
-      const cmdId = cliArgv[1]
+      // B - find cmd to complete
+      const cmdId = commandLineToComplete[1]
       const plugins = new Plugins(this.out)
       await plugins.load()
       let Command = await plugins.findCommand(cmdId)
-      if (!Command) throw new Error(`1`)
+      if (!Command) throw new Error(`Command ${cmdId} not found`)
 
-      // C
-      let cacheKey = 'void'
-      let cacheCompletion : ?Object
-      let [argvArgCount, curIsFlag, curIsFlagValue] = this._calcCurArgvState(argv, Command)
+      // C -
+      // 1. find what is asking to be completed
+      // 2. set any parsable context from exisitng args or flags
+      // 3. set vars needed to build/retrive cache
+      const cmdArgv = commandLineToComplete.slice(2)
+      const cmdArgvCount = cmdArgv.length
+      const cmdCurArgv = cmdArgv[cmdArgvCount - 1]
+      const cmdPreviousArgv = cmdArgv[cmdArgvCount - 2]
+      let [cmdCurArgCount, cmdCurArgvIsFlag, cmdCurArgvIsFlagValue] = this._determineCmdState(cmdArgv, Command)
+      let cacheKey
+      let cacheCompletion
 
-      if (curIsFlag || curIsFlagValue) {
-        const argvFlag = curIsFlagValue ? previousArg : curArg
+      if (cmdCurArgvIsFlag || cmdCurArgvIsFlagValue) {
+        const argvFlag = cmdCurArgvIsFlag ? cmdPreviousArgv : cmdCurArgv
         let {name, flag} = this._findFlagFromWildArg(argvFlag, Command)
-        if (!flag) throw new Error(`6`)
-        cacheKey = flag.name || name || 'no_name'
+        if (!flag) throw new Error(`${argvFlag} is not a valid flag for ${cmdId}`)
+        cacheKey = name || flag.name
         cacheCompletion = flag.completion
       } else {
         const cmdArgs = Command.args || []
         const cmdArgsCount = cmdArgs.length
-        if (argvArgCount > cmdArgsCount || argvArgCount === 0) throw new Error(`2c`)
-        const arg = cmdArgs[argvArgCount - 1]
+        if (cmdCurArgCount > cmdArgsCount || cmdCurArgCount === 0) throw new Error(`Cannot complete arg position ${cmdCurArgCount} for ${cmdId}`)
+        const arg = cmdArgs[cmdCurArgCount - 1]
         cacheKey = arg.name
         cacheCompletion = arg.completion
       }
 
-      // D
-      // load context from flags= and completed args
-
-      // E
+      // build/retrieve & return cache
       if (cacheCompletion && cacheCompletion.options) {
-        const args = this.parsedArgs
-        const flags = this.parsedFlags
-        const ctx = {args, flags, argv: this.argv, out: this.out}
+        // use cacheKey function or fallback to arg/flag name
+        const ctx = {args: this.parsedArgs, flags: this.parsedFlags, argv: this.argv, out: this.out}
         const ckey = cacheCompletion.cacheKey ? await cacheCompletion.cacheKey(ctx) : null
         const key = (ckey || cacheKey)
-        const flagCache = path.join(this.completionsPath, key)
+        const flagCachePath = path.join(this.completionsPath, key)
+
+        // build/retrieve cache
         const duration = cacheCompletion.cacheDuration || 60 * 60 * 24 // 1 day
         const cacheFunc = cacheCompletion.options(ctx)
         const opts = {cacheFn: () => cacheFunc}
-        const options = await ACCache.fetch(flagCache, duration, opts)
+        const options = await ACCache.fetch(flagCachePath, duration, opts)
+
+        // return cache/options
         this.out.log((options || []).join('\n'))
       }
     } catch (err) {
-      // this.out.log(err)
+      // on error, audible 'beep'
       process.stderr.write('\x07')
+      // write to ac log
+      this.writeLogFile(err.message)
     }
   }
 
@@ -130,14 +95,14 @@ export default class ACFoo extends AutocompleteBase {
     return {}
   }
 
-  _calcCurArgvState (argv: Array<string>, Command: Class<Command<*>>): [number, boolean, boolean] {
+  _determineCmdState (argv: Array<string>, Command: Class<Command<*>>): [number, boolean, boolean] {
     let needFlagValueSatisfied = false
     let argIsFlag = false
     let argIsFlagValue = false
     let argsIndex = 0
     let flagName
-    // find number of argv's (including '')
-    // that are not a flag or flag value
+    // find cur index of argv (including empty '')
+    // that are not flags or flag values
     const nthArg = argv.filter(wild => {
       if (wild.match(/^-(-)?/)) {
         // we're a flag
@@ -199,27 +164,5 @@ export default class ACFoo extends AutocompleteBase {
     }).length
 
     return [nthArg, argIsFlag, argIsFlagValue]
-    // unit test this
-    // "ac:foo " 1, false, false
-    // "ac:foo arg" 1, false, false
-
-    // "ac:foo arg " 2, false, false
-    // "ac:foo arg barg" 2, false, false
-
-    // "ac:foo arg barg " 3, false, false
-
-    // (ERROR, but not here, cause this is flag name completion)
-    // "ac:foo arg -f" 1, true, false
-    // "ac:foo arg --f" 1, true, false
-    // "ac:foo arg --foo" 1, true, false
-
-    // "ac:foo arg -s " 1, false, true
-    // "ac:foo arg --space " 1, false, true
-
-    // "ac:foo arg --foo " 2, false, false
-    // "ac:foo arg -f " 2, false, false
-
-    // ??? does this matter yet?
-    // "ac:foo arg --space=" 1, true, false <<???
   }
 }
